81138321e722bfcba559119e9aef3d54ee5e0d69
alex <alex@scriptoid.com>
2024-04-18 11:14:16 +0300

    Update DB setup script

--- test-db.py
+++ test-db.py
@@ -3,20 +3,25 @@ import os
 import pymysql
 
 # Database connection parameters - update as needed
-DB_USER = os.getenv("DB_USER") or "root" 
-DB_PSWD = os.getenv("DB_PSWD") or None 
-DB_HOST = os.getenv("DB_HOST") or "localhost" 
-
-db = pymysql.connect(
-    host=DB_HOST, 
-    user=DB_USER, 
-    password=DB_PSWD, 
-    database="mysql", 
-    cursorclass=pymysql.cursors.DictCursor,
-) 
-
-cursor = db.cursor() 
-cursor.execute("SHOW DATABASES") 
+DB_USER = os.getenv("DB_USER") or "task_logger"
+DB_PSWD = os.getenv("DB_PSWD") or "task_logger"
+DB_NAME = os.getenv("DB_NAME") or "task_logger"
+DB_HOST = os.getenv("DB_HOST") or "localhost"
 
 print("Content-type: text/html\n")
-print("Setup Successful")
+
+try:
+    db = pymysql.connect(
+        host=DB_HOST, 
+        user=DB_USER, 
+        password=DB_PSWD, 
+        database="mysql", 
+        cursorclass=pymysql.cursors.DictCursor,
+    ) 
+
+    cursor = db.cursor() 
+    cursor.execute("SHOW DATABASES") 
+    
+    print("Setup Successful")
+except Exception as e:
+    print("Setup unsuccessful.", e)
